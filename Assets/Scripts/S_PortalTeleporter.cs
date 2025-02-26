f85d7baf61becabce6fda875b086c18abd235782
Romoya42 <155167992+Romoya42@users.noreply.github.com>
2025-02-26 11:15:21 -0500

    Update

--- Assets/Scripts/S_PortalTeleporter.cs
+++ Assets/Scripts/S_PortalTeleporter.cs
@@ -16,18 +16,19 @@ public class PortalTeleporter : MonoBehaviour {
 			Vector3 portalToPlayer = player.position - transform.position;
 			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
+
+			
 			
 			if (dotProduct < 0f)
 			{
 				
-				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
-				rotationDiff += 180;
-				player.Rotate(Vector3.up, rotationDiff);
+				
 
-				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-				player.position = reciever.position + positionOffset;
+				Vector3 positionOffset = portalToPlayer;
+    			player.position = reciever.position + positionOffset;
 
-				playerIsOverlapping = false;
+    			playerIsOverlapping = false;
+
 			}
 			 
 			
