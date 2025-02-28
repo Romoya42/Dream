50e119f89d80c24846250a8467284ae4cc1b5e4f
WinnieThePoop4 <158162266+WinnieThePoop4@users.noreply.github.com>
2025-02-28 10:41:26 -0500

    Revert "Update PauseMenu Blur"
    This reverts commit dd7789ff5b8a8c1bc758c3e4460594dcd1af7069.

--- Assets/Scripts/Menu/S_PauseMenu.cs
+++ Assets/Scripts/Menu/S_PauseMenu.cs
@@ -1,17 +1,10 @@
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 public class S_PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     public static bool isPaused;
     public Canvas cursorCanvas;
-
-    public Volume globalVolume;
-    private DepthOfField dof;
-    private bool toggleDof;
-    public float focusDistance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,7 +25,6 @@ public class S_PauseMenu : MonoBehaviour
     public void PauseGame()
     {
         pauseMenu.SetActive(true);
-        ToggleBackgroundUI();
         cursorCanvas.enabled = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
@@ -43,21 +35,10 @@ public class S_PauseMenu : MonoBehaviour
     public void ResumeGame()
     {
         pauseMenu.SetActive(false);
-        ToggleBackgroundUI();
         cursorCanvas.enabled = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1f;
         isPaused = false;
-    }
-
-    private void ToggleBackgroundUI()
-    {
-        toggleDof = !toggleDof;
-        if (globalVolume.profile.TryGet(out dof))
-        {
-            dof.active = toggleDof;
-            dof.focusDistance.value = focusDistance;
-        }
     }
 }
